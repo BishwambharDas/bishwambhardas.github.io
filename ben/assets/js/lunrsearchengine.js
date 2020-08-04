0e3ea2ff533fbe627025fd2d02ae544ad2e16680
BishwambharDas <68798220+BishwambharDas@users.noreply.github.com>
2020-08-04 08:32:13 +0530

    Copied site contents and put it in source branch

---
+++ ben/assets/js/lunrsearchengine.js
@@ -0,0 +1,99 @@
+
+var documents = [{
+    "id": 0,
+    "url": "http://localhost:4000/ben/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "http://localhost:4000/ben/about",
+    "title": "Mediumish Template for Jekyll",
+    "body": "This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.  Documentation: Please, read the docs here. Questions or bug reports?: Head over to our Github repository! Buy me a coffeeThank you for your support! Your donation helps me to maintain and improve Mediumish . Buy me a coffee Documentation"
+    }, {
+    "id": 2,
+    "url": "http://localhost:4000/ben/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "http://localhost:4000/ben/",
+    "title": "Home",
+    "body": "      Featured:                     All Stories:                                                                                                     ক্ষতিপূরণ …!              :       ক্ষতিপূরণ? খায় না মাথায় দেয়?…পকেটে দেয় বুঝি??:                                                                               Hrishikesh                02 Aug 2020                                            "
+    }, {
+    "id": 4,
+    "url": "http://localhost:4000/ben/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 5,
+    "url": "http://localhost:4000/ben/vinoda-and-vinoda-bihari-ben/",
+    "title": "ক্ষতিপূরণ …!",
+    "body": "2020/08/02 - ক্ষতিপূরণ? খায় না মাথায় দেয়?…পকেটে দেয় বুঝি?? আমাদের কার্টুন নেটওয়ার্ক আর অসওয়াল্ডের দিনে, যখন মা আমার ঝাঁকড়া অবাধ্য চুলগুলো কে কাটাতে নিয়ে যেত…পেতাম…পেতাম ক্ষতি্পূরণ! গাঢ় নীল প্যাকেটের তলায় সোনালী রাংতা মোড়া ক্ষতিপূরণ…! মা-এর কাছে বকুনি খেলে বাবা দিত ক্ষতিপূরণ! সোনালী রাংতা মোড়া ক্ষতিপূরণ, ছোট্ট রঙীন গাড়ীর ক্ষতিপূরণ…ক্ষতিপূরণ বিকেলে তিলপাড়ায় বেড়াতে নিয়ে যাওয়ার প্রতিশ্রুতি! ক্ষতিপূরণ !… খবরের কাগজের পাতা ওল্টাতে গিয়ে অনেক অনেক বার করে পাওয়া যায় এমন একটা শব্দ! মন একটা শব্দ যা সর্বাধিকবার প্রতিশ্রুতি বদ্ধ হয় শাসকের বা শোষকের দ্বারা! তোমার ছেলে মারা গেছে দূর্ঘটনায়? তুমি পাবে ক্ষতিপূরণ! সাপে কামড়ে মারা গেছে তোমার প্রিয়তম? উড়ালপুলের নীচে চাপা পড়েছে তোমার প্রেম? ছিটকে এসে তোমার গায়ে লেগেছে চাপা পড়া থেঁতলানো তোমার সন্তানের রক্ত?? তাতেও তুমি পাবে ক্ষতিপূরণ! কেউ তোমায় বিক্রি করেছে তোমার ‘মুখ-বুক-পেট’ মেপে? তোমায় ছিদ্রওয়ালা মাংসপিন্ড ভেবে?? বরাদ্দ তোমার জন্য ক্ষতিপূরণ! ১,২,৩,৪ সংখ্যার পাশে তিনটি বা চারটি ‘শূন্য’! তার নাম ‘ক্ষতিপূরন’! শব্দটা আজব! না না আভিধানিক স্বীকৃতি আছে বইকী! কিন্তু তার বাইরে? আমি তোমায় ক্ষতিপূরণ দেবো…রাষ্ট্র দেবে…অমুক দেবে তমুক দেবে…ঝোলা ভরা ক্ষতিপূরণ! কিন্তু ‘ক্ষতি’ পুরাতে গেলে আগে তো মাপতে হবে! মাপবে কে?… আমি??তুমি??? রাষ্ট্র না অন্য কোনো তৃতীয় সংস্থা??? পারবো মাপতে তোমার ক্ষতি?? পারবো মাপতে একবোতল আ্যসিড মুখের উপর পড়লে কতটা কষ্ট হয় আমার কেমিষ্ট্রী ল্যাবে ভুল করে হাতে পড়ে যাওয়া লঘু বা লঘুতর আ্যসিডের যন্ত্রনার নিক্তি দিয়ে? পারবো বুঝতে ঠিক কতটা কষ্ট হয় যখন কারো এক্কাদোক্কা খেলার বয়সী বোনের ক্ষতবিক্ষত দেহ উদ্ধার হয় কোনো ধানক্ষেত থেকে? মাপতে পারবো ঠিক কতটা আর্তনাদ, চিৎকার, হাহাকার লুকিয়ে আছে লোকভর্তি উঠোনে একলা এক্কেবারে চুপ মায়ের মনে, মেয়ের গণধর্ষিতা সাদাকাপড়ে মোড়া টাটকা বা বাসী রক্তের ছোপওয়ালা ‘বডি’টাকে দেখে??? “দেশের লেগে মইরেছে গো তোমার ছে্ল্যা…” বলে হাতে গান্ধীজির হাসিমুখওয়ালা কাগজের আয়তকার টুকরোগুলো হাতে ধরালে কি কমে যায় সদ্য ছেলে হারানোর কষ্ট??? ক্ষতি তো পুরাতে পারবোনা! তাই ক্ষতিপূরণ!…হতে পারে…! স্বীকার্য যে দূর্ঘটনা আমরা আটকাতে পারবো না সবসময়! অপূরনীয় ক্ষতির সামনে দাঁড়িয়ে তাই ক্ষতিপূরণই আমাদের একমাত্র ঠাঁই! কিন্তু ক্ষতি যারা করলো? তাদের খোলা আকাশের নীচে বুকভরে নিঃশ্বাস নিতে দেখলে তোমার যে রক্তক্ষরণ হয় প্রতিনিয়ত… তাতেও কী মলম লাগাতে পারবোনা আমরা? লাগাবোনা আমরা! তাই আছে মুখ-বন্ধের ক্ষতিপূরণ! তোমার জিভ সেলাই করার ক্ষতিপূরণ! “কোর্ট-কাছারির ঝামেলায় গিয়ে আর লাভ কী? তার চেয়ে বরং…”এর ক্ষতিপূরণ!!! “ যা হবার হয়েগেছে! ভাগ্যে ছিল! এখন বরং ভালো ছেলে দেখে বিয়ে দিয়ে দাও… খরচাপাতি নিয়ে ভেবোনা… হেঁ হেঁ”র ক্ষতিপূরণ!!! “ভাগ্যের লিখন কী কেউ খন্ডাতে পারে?”র ক্ষতিপূরণ!!… ক্ষতিপূরণের শেষ নাই! তোমার অক্ষতযোনি দায়িত্ব নিয়ে ছিন্নভিন্ন করার ক্ষতি আমি পুরাতে পারবোনা… পারবো তোমার অক্ষত-তুমুল ধারালো ঠোঁট দুটোকে সেলাই করে আমার ঘরের লোককে বাঁচাতে! তোমার চোখে আবার আমি ফেরাতে পারবোনা আগের হাসি… পারবোনা কোনো অলৌকিক জাদুকাঠি দিয়ে তোমার চামড়া আবার টানটান করতে… পারবোনা লোকের ফিসফাস-গুজগুজ থেকে তোমার কানজোড়াকে আগলে রাখতে… পারবো তোমার আধা বা আরোকম চিকিৎসার বিনিময়ে তোমার কেস ফাইলটাকে ‘ক্লোজড’ করতে! পারবো না তোমার হাহাকারের তলানি খুঁজতে… পারবো তোমার পকেটের তলানি বুঝে সেটা ভর্তি করতে…!!!! অতএব ক্ষতিপূরণ…! ‘ক্ষতি’ পুরাতে পারবোনা জেনেও ক্ষতিপূরণ! ‘ক্ষতি’গুলো বরং জমুক… ঘেন্না হয়ে… জ্বালিয়ে দিক…আমি নিরাপদ আশ্রয়ে আরামের চাদর মুড়ি দিয়ে ঘুম দিই… ! গভীর খুব গভীর ঘুম…চোখ কান সম্পুর্ণ বন্ধ রাখার ঘুম! আর তুমি ঝলসাও তোমার যন্ত্রনায়,তোমার কষ্টে… ঘৃতাহুতির জন্য রইলোই ‘ক্ষতিপূরণ’!! "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
